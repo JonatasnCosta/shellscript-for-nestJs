47f4e005e97ec11536f6ee72cd180eb4ff546aaa
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-04-14 07:22:38 -0300

    Completed automation and npm run process

--- nest.sh
+++ nest.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-#Automation to create an app in nestJs
-
 echo "What is the name of the project?"
 read projectName
 
 nest new $projectName
+
+projectPath=$(pwd) 
+
+cd $projectPath/$projectName
+
+code . 
+
+cd $projectPath/$projectName/src
+
+rm app.controller.spec.ts  app.controller.ts app.service.ts  app.module.ts
+
+touch app.module.ts
+
+appModuleCleaning="
+import { Module } from '@nestjs/common'; 
+
+@Module(
+{ imports: [], 
+controllers: [], 
+providers: [], 
+}) 
+
+export class AppModule {}"
+
+echo "$appModuleCleaning" > app.module.ts
+
+npm run start:dev
+
+
+
