6cbeb17daa0f2e4281cd443b65734a129338d393
JonatasCosta1983 <jonathanevescosta@gmail.com>
2023-05-12 18:27:42 -0300

    Modificado o nestValidation.sh

--- nestValidation.sh
+++ nestValidation.sh
@@ -3,3 +3,22 @@
 #Script to install the validation pipe
 
 npm i --save class-validator class-transformer
+
+projectPath=$(pwd) 
+
+cd $projectPath/src
+
+rm main.ts 
+
+mainCleaning="import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();"
+
+echo "$mainCleaning" > main.ts
